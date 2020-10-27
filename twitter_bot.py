f71bd1627680fa8b6b18b3a7456c89144c5387d1
su-yiting <forstudentsg@gmail.com>
2020-10-27 15:52:34 +0800

    change project version

--- twitter_bot.py
+++ twitter_bot.py
@@ -1,52 +1,74 @@
 import tweepy
+import logging
 import time
 import datetime
 import os
 from os import environ
 
 now = datetime.datetime.now()
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
 
 print('This is a twitter bot')
 
-CONSUMER_KEY = environ['CONSUMER_KEY']
-CONSUMER_SECRET = environ['CONSUMER_SECRET']
-ACCESS_KEY = environ['ACCESS_KEY']
-ACCESS_SECRET = environ['ACCESS_SECRET']
+# CONSUMER_KEY = environ['CONSUMER_KEY']
+# CONSUMER_SECRET = environ['CONSUMER_SECRET']
+# ACCESS_KEY = environ['ACCESS_KEY']
+# ACCESS_SECRET = environ['ACCESS_SECRET']
+
+# auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+# auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+# api = tweepy.API(auth, wait_on_rate_limit=True)
+
+consumer_key = 'jjPiGCsj8Wk5wu7EqDPAqstsb'
+consumer_secret = 'Ed5ba1z2zjCevMtCFwNm3LxrcwdumzOOI6irsBq2RQkrUSUhfc'
+access_token = '1316692888550404097-69772k4vmEPgQt9G8ECASFqpBusRyT'
+access_token_secret = '5Hm4sdbJB4ScyRazWuppvmKSW89iAYmy8u3aG56BLdmsr'
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
 
 
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
-api = tweepy.API(auth, wait_on_rate_limit=True)
-
-def retrieve_last_seen_id(file_name):
-    f_read = open(file_name, 'r')
-    last_seen_id = int(f_read.read().strip())
+def retrieve_hastags(file_name):
+    with open(file_name, 'r') as f_read:
+        hastags_name = [hastag.strip() for hastag in f_read]
     f_read.close()
-    return last_seen_id
+    return hastags_name
 
-def store_last_seen_id(last_seen_id,file_name):
-    f_write = open(file_name, 'w')
-    f_write.write(str(last_seen_id))
-    f_write.close()
-    return
 
 FILE_NAME_FAV = 'last_fav_tweet_id.txt'
 
-def fav_tweet():
-    print('Retrieving tweets...')
-    last_seen_id = retrieve_last_seen_id(FILE_NAME_FAV)
-    mentions = api.mentions_timeline(last_seen_id, tweet_mode = 'extended')
-    for mention in reversed(mentions):
-        if not mention:
-            return
-        print(str(mention.id) + ' - ' + mention.full_text, flush=True)
-        last_fav_tweet = mention.id
-        store_last_seen_id(last_fav_tweet,FILE_NAME_FAV)
-        print('found @nog_nuus', flush = True)
-        print('fav-ing and retweeting tweet...', flush = True)
-        api.create_favorite(mention.id)
-        api.retweet(mention.id)
 
-while True:
-    fav_tweet()
-    time.sleep(15)
+def retweet_tweets_with_hashtag(api, need_hashtags):
+    if type(need_hashtags) is list:
+        search_query = f"{need_hashtags} -filter:retweets"
+        tweets = api.search(q=search_query, lang="en", tweet_mode='extended')
+        n = 0
+        for tweet in tweets:
+            if n > 3:
+                break
+            hashtags = [i['text'].lower()
+                        for i in tweet.__dict__['entities']['hashtags']]
+            try:
+                need_hashtags = [hashtag.strip('#')
+                                 for hashtag in need_hashtags]
+                need_hashtags = list(need_hashtags)
+                if set(hashtags) & set(need_hashtags):
+                    if tweet.user.id != api.me().id:
+                        api.retweet(tweet.id)
+                        logger.info(f"Retweeted tweet from {tweet.user.name}")
+                        n += 1
+                        time.sleep(60)
+            except tweepy.TweepError:
+                logger.error("Error on retweet", exc_info=True)
+    else:
+        logger.error(
+            "Hashtag search terms needs to be of type list", exc_info=True)
+        return
+
+
+retrieve_hashtags = retrieve_hastags(FILE_NAME_FAV)
+for h in retrieve_hashtags:
+    retrieve_hashtag = ['#'+h]
+    retweet_tweets_with_hashtag(api, retrieve_hashtag)
+    time.sleep(100)
